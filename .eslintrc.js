340b3985553f7215334fabe421223974978823e6
juerui <juerui@vip.qq.com>
2025-04-24 19:12:11 +0800

    基础Demo

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    // 可以根据需要添加自定义规则
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}; 
