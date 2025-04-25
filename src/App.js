f09bdb1c1b63101823213c0437788b781ac63d20
yhsy <juerui@vip.qq.com>
2025-04-25 10:36:43 +0800

    优化

--- src/App.js
+++ src/App.js
@@ -3,20 +3,60 @@ import React, { memo, useLayoutEffect, useRef } from "react";
 import "./App.css";
 
 import anBgImg from "./assets/an/egg-ga/images/d69feb5cd469e57a03d1fbd78343b5cf.png";
-// import eggJson from "./assets/an/egg-ga/egg.json";
-import eggJson from "./assets/an/egg-ga/egg-png.json";
+
+import eggJson from "./assets/an/egg-ga/egg.json";
+import eggJsonTiny from "./assets/an/egg-ga/egg-tiny.json";
+
+import eggJsonPng from "./assets/an/egg-ga/egg-png.json";
+import eggJsonPngTiny from "./assets/an/egg-ga/egg-png-tiny.json";
 
 import AnGalacean from "./components/an-ga";
 
 function App(props = { show: true }) {
-
   return (
     <div className="App">
-      <h1>Demo</h1>
-      <div className="animation-container" style={{width: "475px", height: "367px"}}>
-        <AnGalacean anJson={eggJson} bg={anBgImg} />
-      </div>
-      
+      <h1>Galacean Demo</h1>
+      <ul
+        className="an-list"
+      >
+        <li>
+          <h3>默认(webp(0.98M)/png(0.88M))兼容加载</h3>
+          <div
+            className="animation-container"
+            style={{ width: "475px", height: "367px" }}
+          >
+            <AnGalacean anJson={eggJson} bg={anBgImg} />
+          </div>
+        </li>
+        <li>
+        {/* <h3>默认-压缩(webp/png)1.43M</h3> */}
+        <h3>默认-压缩(webp(0.77M)/png(0.69M))兼容加载</h3>
+          <div
+            className="animation-container"
+            style={{ width: "475px", height: "367px" }}
+          >
+            <AnGalacean anJson={eggJsonTiny} bg={anBgImg} />
+          </div>
+        </li>
+        <li>
+        <h3>Png格式(png)0.88M</h3>
+          <div
+            className="animation-container"
+            style={{ width: "475px", height: "367px" }}
+          >
+            <AnGalacean anJson={eggJsonPng} bg={anBgImg} />
+          </div>
+        </li>
+        <li>
+        <h3>Png格式-压缩(png)0.69M</h3>
+          <div
+            className="animation-container"
+            style={{ width: "475px", height: "367px" }}
+          >
+            <AnGalacean anJson={eggJsonPngTiny} bg={anBgImg} />
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
